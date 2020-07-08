93635fef0d893e987206451eb858ea5aa0f9aacd
SaiyemBejoy <jsbejoy96@gmail.com>
2020-07-08 21:11:41 +0600

    Implemented Array

---
+++ Data_Structures/Data_Structures/Array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    class Array
+    {
+        private int[] items;
+        private int count;
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(items[i]);
+        }
+
+        public void insert(int item)
+        {
+            if (items.Length == count)
+            {
+                int[] newItems = new int[count * 2];
+                for (int i = 0; i < count; i++)
+                    newItems[i] = items[i];
+
+                items = newItems;
+            }
+
+            items[count++] = item;
+        }
+
+        public void removeAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new System.ArgumentException("Index is undefined");
+
+            for (int i = index; i < count; i++)
+                items[i] = items[i + 1];
+
+            count--;
+        }
+
+        public int indexOf(int item)
+        {
+            for (int i = 0; i < count; i++)
+                if (items[i] == item)
+                    return i;
+            return -1;
+        }
+    }
+}
---
+++ Data_Structures/Data_Structures/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array numbers = new Array(3);
+            numbers.insert(10);
+            numbers.insert(20);
+            numbers.insert(30);
+            numbers.insert(40);
+            Console.WriteLine(numbers.indexOf(20));
+            numbers.print();
+        }
+    }
+}
