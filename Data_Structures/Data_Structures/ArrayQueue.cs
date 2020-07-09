483f72c17bbee7f85635dcbf9369aa315f4e7be9
SaiyemBejoy <jsbejoy96@gmail.com>
2020-07-09 20:46:28 +0600

    Implemented Queue using array

---
+++ Data_Structures/Data_Structures/ArrayQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    class ArrayQueue
+    {
+        private int[] items;
+        private int rear;
+        private int front;
+        private int count;
+
+        public ArrayQueue(int capacity)
+        {
+            items = new int[capacity];
+        }
+
+        public void enqueue(int item)
+        {
+            if (count == items.Length)
+                throw new System.Exception("Queue is full");
+
+            items[rear] = item;
+            rear = (rear + 1) % items.Length;
+            count++;
+        }
+
+        public int dequeue()
+        {
+            if (empty())
+                throw new System.Exception("Queue is empty");
+
+            var item = items[front];
+            items[front] = 0;
+            front = (front + 1) % items.Length;
+            count--;
+            return item;
+        }
+
+        public bool empty()
+        {
+            return count == 0;
+        }
+
+        public int peek()
+        {
+            return items[front];
+        }
+    }
+}
--- Data_Structures/Data_Structures/Program.cs
+++ Data_Structures/Data_Structures/Program.cs
@@ -10,18 +10,11 @@ namespace Data_Structures
     {
         static void Main(string[] args)
         {
-            var stack = new StackViaArray();
-            stack.push(10);
-            stack.push(20);
-            stack.push(30);
-            stack.push(40);
-            stack.push(50);
-            var top = stack.peek();
-            Console.WriteLine("Peek: "+top);
-            while (!stack.empty())
-            {
-                Console.WriteLine(stack.pop());
-            }
+            var queue = new ArrayQueue(5);
+            queue.enqueue(10);
+            queue.enqueue(20);
+            queue.enqueue(30);
+            Console.WriteLine(queue.peek());
         }
     }
 }
